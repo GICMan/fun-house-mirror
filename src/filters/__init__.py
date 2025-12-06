0f064e61988a6ca0d67a753bb7383d6326fc997b
Matthew Tallarico <mst3329@g.rit.edu>
2025-12-06 18:31:36 -0500

    changed name of dummy

--- src/filters/__init__.py
+++ src/filters/__init__.py
@@ -1,6 +1,6 @@
 from .head_squish_1 import HeadSquish1
 from .big_traps_1 import BigTraps1
-from .dummy import Dummy1
+from .long_head import LongHead
 from .sid_face import SidFace
 from .small_waist import SmallWaist
 from .noodle_arms import NoodleArms1
@@ -9,7 +9,7 @@ def init_filters(frame_size):
     return [
         HeadSquish1(frame_size),
         BigTraps1(frame_size),
-        Dummy1(frame_size),
+        LongHead(frame_size),
         SidFace(frame_size),
         SmallWaist(frame_size),
         NoodleArms1(frame_size),
