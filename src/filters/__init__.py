bc6197cc2e47259f76eaf9c138898b097e464138
Gabriel Casselman <gicasselman1@gmail.com>
2025-12-07 17:02:57 -0500

    hid unused filters

--- src/filters/__init__.py
+++ src/filters/__init__.py
@@ -1,16 +1,17 @@
-from .head_squish_1 import HeadSquish1
+# from .head_squish_1 import HeadSquish1
 from .big_traps_1 import BigTraps1
-from .long_head import LongHead
+# from .long_head import LongHead
 from .sid_face import SidFace
 from .small_waist import SmallWaist
 from .noodle_arms import NoodleArms1
 from .small_mouth import SmallMouth
 
+
 def init_filters(frame_size):
     return [
-        HeadSquish1(frame_size),
+        # HeadSquish1(frame_size),
         BigTraps1(frame_size),
-        LongHead(frame_size),
+        # LongHead(frame_size),
         SidFace(frame_size),
         SmallWaist(frame_size),
         NoodleArms1(frame_size),
